6854b27753a6cdcfc5eda619d4a8abf256be2555
Dimitri Stamos <stamosd275@gmail.com>
2022-03-11 23:51:41 -0500

    Cleanup & add previous guess & win condition

--- Hangman.py
+++ Hangman.py
@@ -2,33 +2,46 @@ import random
 
 namebank = ["Brian", "Stevo", "Wayne", "Katie", "Tegan", "Monte"]
 board = ["_", "_", "_", "_", "_"]
-randName = namebank[random.randint(0, len(namebank) - 1)]
+guessbank = []
+randName = namebank[random.randint(0, len(namebank) - 1)].lower()
+win = False
 
 print("Welcome to hangman! Guess by entering a letter, and try to guess the full word!")
 
-while True:
-    guess = input("Enter any letter (or -1 to quit): ")
+print(randName)
+
+while not win:
+    guess = input("\nEnter any letter (or -1 to quit): ")
+
+    try:
+        if int(guess) == -1:
+            print("\n\nBye bye!!")
+            break
+    except:
+        guess = guess.lower()
 
     if guess in randName:
         index = 0
         for letter in randName:
-            if letter == guess:
+            if letter.lower() == guess:
                 board[index] = guess
                 print(board)
             index += 1
             if ''.join(board) == randName:
-                print("You win!!!")
+                print("\n\nYou win!!!")
+                win = True
                 break
-    elif int(guess) == -1:
-        print("Bye bye!")
-        break
     else:
-        print("Incorrect, try again.")
+        print("\nIncorrect, try again.")
+    
+    if guess not in guessbank and not win:
+        guessbank.append(guess)
+        print("\nThis is what you have guessed so far: ")
+        print(guessbank)
 
 exit()
 
 '''Ideas:
-Bank of guessed letters
 Build a board
 Add strikes
 '''
